beb06fe8e46f09aad0e780669b74a5208a4b441a
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-02-11 11:45:52 +0200

    Create double_char.py
    Solve task 7 from exercise

---
+++ Basic Syntax, Conditional Statements and Loops_Exercise/double_char.py
@@ -0,0 +1,13 @@
+while True:
+    word = input()
+    if word == 'End':
+        break
+
+    if word == 'SoftUni':
+        continue
+
+    for ch in word:
+        print(f"{ch}{ch}", end='')
+
+    print()
+    
