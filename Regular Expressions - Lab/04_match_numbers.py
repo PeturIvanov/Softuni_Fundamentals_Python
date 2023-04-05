4a56200a5e4ad769907d7850e2537e1248602dc0
PeturIvanov <116626395+PeturIvanov@users.noreply.github.com>
2023-04-05 17:15:49 +0300

    Create 04_match_numbers.py
    Solve problem 4 from Regular Expressions - Lab

---
+++ Regular Expressions - Lab/04_match_numbers.py
@@ -0,0 +1,7 @@
+import re
+
+pattern = r"(^|(?<=\s))-?([0]|[1-9][0-9]*)(\.\d+)?($|(?=\s))"
+text = input()
+result = re.finditer(pattern, text)
+for match in result:
+    print(match.group(), end=" ")
